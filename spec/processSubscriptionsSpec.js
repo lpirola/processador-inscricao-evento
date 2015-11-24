5407eff325cac43c418266a5eb24288f3b3c6adb
Lucas Pirola <lucaspirola@gmail.com>
2015-11-24 20:18:44 -0200

    Adicionado primeiro bloco de testes utilizando jasmine

---
+++ spec/processSubscriptionsSpec.js
@@ -0,0 +1,8 @@
+describe('Config, read and process subscriptions', function() {
+	describe('SMTP server is not configured', function() {
+		it('Given SMTP server details is not filled', function() {});
+		it('When Developer try to process all subscriptions', function() {});
+		it('Then Process must stop', function() {});
+		it('And output a message with information required', function() {});
+	});
+});
