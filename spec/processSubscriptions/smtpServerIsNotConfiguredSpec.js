a3c7f15db58cf02007f36e68f3e45759d3532d1d
Lucas Pirola <lucaspirola@gmail.com>
2015-11-27 10:04:02 -0200

    Remove dependência do Sinon para Stub nos testes

--- spec/processSubscriptions/smtpServerIsNotConfiguredSpec.js
+++ spec/processSubscriptions/smtpServerIsNotConfiguredSpec.js
@@ -1,26 +1,27 @@
 import mainCommand from '../../src'
-var sinon = require('sinon').sandbox.create()
-var stubExit, stubWrite
+// var sinon = require('sinon').sandbox.create()
+// var stubExit, stubWrite
 describe('Config, read and process subscriptions', () => {
 	describe('SMTP server is not configured', () => {
+		beforeAll(() => {
+			spyOn(process, 'exit')
+			spyOn(process.stdout, 'write')
+			mainCommand.parse(['./node_modules/.bin/babel-node', 'src', 'check']);
+		})
 		it('Given SMTP server details is not filled', () => {
 			expect(process.env['MAIL_SERVICE']).toBeUndefined();
 			expect(process.env['MAIL_USER']).toBeUndefined();
 			expect(process.env['MAIL_PASS']).toBeUndefined();
 		});
 		it('When Developer try to process all subscriptions', () => {
-			stubExit = sinon.stub(process, 'exit');
-			stubWrite = sinon.stub(process.stdout, 'write');
-			mainCommand.parse(['./node_modules/.bin/babel-node', 'src', 'check']);
+			// mainCommand.parse(['./node_modules/.bin/babel-node', 'src', 'check']);
 		});
 		it('Then a message must be output with information required', () => {
-			var output = stubWrite.args;
-			expect(output[0])
+			expect(process.stdout.write.calls.argsFor(0))
 				.toEqual(['Os dados para configuração do envio do email não foram informados']);
 		});
 		it('And process must stop', () => {
-			expect(stubExit.calledOnce).toBe(true);
-			sinon.restore();
+			expect(process.exit.calls.count()).toBe(1);
 		});
 	});
 });
