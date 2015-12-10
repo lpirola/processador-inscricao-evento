201d2cade38bb79b01056460c4a9f8f44fcabd74
Lucas Pirola <lucaspirola@gmail.com>
2015-12-10 15:47:28 -0200

    Melhora tratamento do email encontrado na planilha

--- src/rules/index.js
+++ src/rules/index.js
@@ -12,7 +12,7 @@ class Rule {
 
 	filter (row) {
 		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		return !re.test(row['e-mail'])
+		return !re.test(row['e-mail'].trim())
 	}
 
 	action (row, done2) {
